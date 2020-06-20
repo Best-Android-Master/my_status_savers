10529b35e4513610746f182f30677b0014019579
Zindagi123 <developeriam027@gmail.com>
2020-06-20 10:00:14 -0700

    ok

---
+++ mylibrary/src/main/java/com/turab/mylibrary/XYZ.java
@@ -0,0 +1,11 @@
+package com.turab.mylibrary;
+
+import android.util.Log;
+import android.widget.Toast;
+
+public class XYZ {
+    public void func(String s)
+    {
+        Log.i("okay",s);
+    }
+}
